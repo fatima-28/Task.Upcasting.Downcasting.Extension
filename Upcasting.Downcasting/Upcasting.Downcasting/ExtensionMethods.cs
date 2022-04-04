f3e964b46e19ebc0ca62032219007f5e48216606
fatima-28 <2n6gddb@code.edu.az>
2022-04-05 00:45:27 +0400

    task added

---
+++ Upcasting.Downcasting/Upcasting.Downcasting/ExtensionMethods.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Upcasting.Downcasting
+{
+    public static class ExtensionMethods
+    {
+
+
+        public static bool CustomContains(this string str)
+        {//ab
+            int LetterCount = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                LetterCount++;
+            }
+            if (LetterCount > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool CustomContains(this char chr)
+        {
+            int LetterCount = 0;//a
+            for (int i = 0; i < 1; i++)
+            {
+                LetterCount++;
+            }
+            if (LetterCount == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsPrime(this int number)
+        {
+            int count = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Upcasting.Downcasting/Upcasting.Downcasting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Upcasting.Downcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Fatima";
+            Console.WriteLine(text.CustomContains());
+            char character = 'a';
+            Console.WriteLine(character.CustomContains());
+            int number = 12;
+            Console.WriteLine(number.IsPrime());
+
+        }
+
+    }
+}
